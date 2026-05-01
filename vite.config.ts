b94bd803aff77bc5fb4c2a9f98b069162405a0bb
FidelMolday <fidelouma363@gmail.com>
2026-05-01 18:08:34 +0300

    production ready

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "*",
+    host: "::",
     port: 8080,
+    allowedHosts: ["riftnova-s-future.onrender.com"],
     hmr: {
       overlay: false,
     },
